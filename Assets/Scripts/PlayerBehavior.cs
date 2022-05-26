e3dcfb2b9c853dd2e6632c8fd8f0ba3960d1f5e4
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-05-26 09:08:05 -0300

    Chicken Egg Game

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public GameObject prefab, spawnPoint;
+    GameObject clone;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            clone = Instantiate(prefab);
+            clone.transform.position = spawnPoint.transform.position - Vector3.forward;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed, rotationSpeed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+    }
+}
